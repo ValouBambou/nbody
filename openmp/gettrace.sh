c717ee36af87eb56a0565f65e529d4afb4a6cef1
Valentin Trophime <theval358@gmail.com>
2021-11-08 10:27:25 +0100

    machinesASR

--- openmp/gettrace.sh
+++ openmp/gettrace.sh
@@ -4,9 +4,9 @@ if [ $# -eq 0 ] ; then
     echo "usage: nbody_barnes_hut nparticules ntime (implem1 or implem2) nthreads"
 	exit 0
 fi
-export OMP_NUM_THREADS=$4
+export OMP_NUM_THREADS=$5
 make clean
 make CC="eztrace_cc gcc" $1
-eztrace -o /tmp -t omp ./$1 $2 $3
+eztrace -o /tmp -t omp ./$1 $2 $3 $4
 eztrace_convert -o /tmp/eztrace_output_anquetil_trophime /tmp/${USER}_eztrace_log_rank_1
 vite /tmp/eztrace_output_anquetil_trophime.trace
